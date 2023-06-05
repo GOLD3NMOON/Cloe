59afd74031130f1f9296418709bc1050fab5992f
Gold3nmoon <124485664+GOLD3NMOON@users.noreply.github.com>
2023-06-05 05:26:40 -0300

    update events/onready.py

--- events/onready.py
+++ events/onready.py
@@ -9,18 +9,18 @@ class OnReady(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("╭─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╮")
-        print("   Bot conectado e pronto para uso   ")
-        print("╰─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╯")
-        print(f"     Nome do bot: {self.bot.user.name}   ")
-        print(f"     ID do bot: {self.bot.user.id}   ")
-        print(f"     Quantidade de servidores: {len(self.bot.guilds)}   ")
-        print("╭─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╮")
-        print("   Servidores conectados:   ")
-        for guild in self.bot.guilds:
-            print(f"     Nome do servidor: {guild.name}   ")
-            print(f"     ID do servidor: {guild.id}   ")
-            print("╰─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╯")
+        print("━━━━━━━━━━━━━━━━")
+        print(" Bot conectado e pronto para uso ")
+        print("━━━━━━━━━━━━━━━━")
+        print(f" Nome do bot: {self.bot.user.name} ")
+        print(f" ID do bot: {self.bot.user.id} ")
+        print(f" Quantidade de servidores: {len(self.bot.guilds)} ")
+        print("━━━━━━━━━━━━━━━━")
+
+        await self.bot.change_presence(
+            status=discord.Status.idle,
+            activity=discord.Activity(type=discord.ActivityType.listening, name="brilho da lua.")
+        )
 
 
 def setup(bot: commands.Bot):
